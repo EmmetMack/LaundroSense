9b1d0ce91ec545f7cf9597c2762d142a2dc9c03b
EmmetMack <emack@andrew.cmu.edu>
2020-05-03 17:40:37 -0400

    Think notifications are pretty much set up

--- app/src/main/java/com/example/laundrosense/MainActivity.java
+++ app/src/main/java/com/example/laundrosense/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.laundrosense;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.core.app.NotificationCompat;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,6 +18,10 @@ import android.view.MenuItem;
 import android.view.View;
 
 import com.example.laundrosense.ui.main.SectionsPagerAdapter;
+
+import static androidx.core.app.NotificationCompat.VISIBILITY_PUBLIC;
+
+import android.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,5 +43,30 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        String id = "LaundroSenseChannel";
+        private void createNotificationChannel()  {
+            // Create the NotificationChannel, but only on API 26+ because
+            // the NotificationChannel class is new and not in the support library
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                CharSequence name = "channel";
+                String description = "Notification Channel for our Laundry Sensing Application";
+                int importance = NotificationManager.IMPORTANCE_DEFAULT;
+                NotificationChannel channel = new NotificationChannel(id, name, importance);
+                channel.setDescription(description);
+                // Register the channel with the system; you can't change the importance
+                // or other notification behaviors after this
+                NotificationManager notificationManager = getSystemService(NotificationManager.class);
+                notificationManager.createNotificationChannel(channel);
+            }
+        }
     }
+
+    NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "LaundroSenseChannel")
+        .setContentTitle("Laundry Done")
+        .setContentText("Washer is done")
+        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        .setVisibility(VISIBILITY_PUBLIC)
+            .setSmallIcon(R.drawable.notification_icon)
+            .setBadgeIconType(NotificationCompat.BADGE_ICON_SMALL);
 }
