9918264136a50a91bd030a1cd93c5f0d262e5a77
EmmetMack <emack@andrew.cmu.edu>
2020-05-04 12:57:51 -0400

    Working on layout, need to mess with it a little more

--- app/src/main/java/com/example/laundrosense/MainActivity.java
+++ app/src/main/java/com/example/laundrosense/MainActivity.java
@@ -15,52 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-//        ViewPager viewPager = findViewById(R.id.view_pager);
-//        viewPager.setAdapter(sectionsPagerAdapter);
-//        TabLayout tabs = findViewById(R.id.tabs);
-//        tabs.setupWithViewPager(viewPager);
-//        FloatingActionButton fab = findViewById(R.id.fab);
-
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-
-//        createNotificationChannel();
     }
-
-    String channelID   = "LaundroSenseChannel";
-//    private void createNotificationChannel() {
-//        // Create the NotificationChannel, but only on API 26+ because
-//        // the NotificationChannel class is new and not in the support library
-//        NotificationManager notificationManager = null;
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            CharSequence name = "channel";
-//            String description = "Notification Channel for our Laundry Sensing Application";
-//            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-//            NotificationChannel channel = new NotificationChannel(channelID, name, importance);
-//            channel.setDescription(description);
-//            // Register the channel with the system; you can't change the importance
-//            // or other notification behaviors after this
-//            notificationManager = getSystemService(NotificationManager.class);
-//            notificationManager.createNotificationChannel(channel);
-//        }
-
-//        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "LaundroSenseChannel")
-//                .setContentTitle("Laundry Done")
-//                .setContentText("Washer is done")
-//                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-//                .setVisibility(VISIBILITY_PUBLIC)
-//                .setBadgeIconType(NotificationCompat.BADGE_ICON_SMALL);
-//
-//        int notificationID = 1;
-//
-//        notificationManager.notify(notificationID, builder.build());
-//        notificationID ++;
 
         public void sendNotification(View view) {
 
