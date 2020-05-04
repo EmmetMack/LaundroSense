40dededa220b86fc47a476201a4d48cff2fa904b
EmmetMack <emack@andrew.cmu.edu>
2020-05-04 12:47:50 -0400

    Cleaning up code and getting notificaitons to work

--- app/src/main/java/com/example/laundrosense/MainActivity.java
+++ app/src/main/java/com/example/laundrosense/MainActivity.java
@@ -1,27 +1,13 @@
 package com.example.laundrosense;
 
-import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.os.Build;
+import android.content.Context;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.tabs.TabLayout;
-
 import androidx.core.app.NotificationCompat;
-import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-
-import com.example.laundrosense.ui.main.SectionsPagerAdapter;
-
-import static androidx.core.app.NotificationCompat.VISIBILITY_PUBLIC;
-
-import android.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,44 +15,81 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
+        //SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+//        ViewPager viewPager = findViewById(R.id.view_pager);
+//        viewPager.setAdapter(sectionsPagerAdapter);
+//        TabLayout tabs = findViewById(R.id.tabs);
+//        tabs.setupWithViewPager(viewPager);
+//        FloatingActionButton fab = findViewById(R.id.fab);
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
-        String id = "LaundroSenseChannel";
-        private void createNotificationChannel()  {
-            // Create the NotificationChannel, but only on API 26+ because
-            // the NotificationChannel class is new and not in the support library
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                CharSequence name = "channel";
-                String description = "Notification Channel for our Laundry Sensing Application";
-                int importance = NotificationManager.IMPORTANCE_DEFAULT;
-                NotificationChannel channel = new NotificationChannel(id, name, importance);
-                channel.setDescription(description);
-                // Register the channel with the system; you can't change the importance
-                // or other notification behaviors after this
-                NotificationManager notificationManager = getSystemService(NotificationManager.class);
-                notificationManager.createNotificationChannel(channel);
-            }
+//        createNotificationChannel();
+    }
+
+    String channelID   = "LaundroSenseChannel";
+//    private void createNotificationChannel() {
+//        // Create the NotificationChannel, but only on API 26+ because
+//        // the NotificationChannel class is new and not in the support library
+//        NotificationManager notificationManager = null;
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            CharSequence name = "channel";
+//            String description = "Notification Channel for our Laundry Sensing Application";
+//            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+//            NotificationChannel channel = new NotificationChannel(channelID, name, importance);
+//            channel.setDescription(description);
+//            // Register the channel with the system; you can't change the importance
+//            // or other notification behaviors after this
+//            notificationManager = getSystemService(NotificationManager.class);
+//            notificationManager.createNotificationChannel(channel);
+//        }
+
+//        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "LaundroSenseChannel")
+//                .setContentTitle("Laundry Done")
+//                .setContentText("Washer is done")
+//                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+//                .setVisibility(VISIBILITY_PUBLIC)
+//                .setBadgeIconType(NotificationCompat.BADGE_ICON_SMALL);
+//
+//        int notificationID = 1;
+//
+//        notificationManager.notify(notificationID, builder.build());
+//        notificationID ++;
+
+        public void sendNotification(View view) {
+
+            //Get an instance of NotificationManager//
+
+            NotificationCompat.Builder mBuilder =
+                    new NotificationCompat.Builder(this)
+                            .setSmallIcon(R.mipmap.ic_launcher)
+                            .setContentTitle("My notification")
+                            .setContentText("Hello World!");
+
+
+            // Gets an instance of the NotificationManager service//
+
+            NotificationManager mNotificationManager =
+
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+            // When you issue multiple notifications about the same type of event,
+            // it’s best practice for your app to try to update an existing notification
+            // with this new information, rather than immediately creating a new notification.
+            // If you want to update this notification at a later date, you need to assign it an ID.
+            // You can then use this ID whenever you issue a subsequent notification.
+            // If the previous notification is still visible, the system will update this existing notification,
+            // rather than create a new one. In this example, the notification’s ID is 001//
+
+//            NotificationManager.notify();
+
+            mNotificationManager.notify(001, mBuilder.build());
         }
     }
 
-    NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "LaundroSenseChannel")
-        .setContentTitle("Laundry Done")
-        .setContentText("Washer is done")
-        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-        .setVisibility(VISIBILITY_PUBLIC)
-            .setSmallIcon(R.drawable.notification_icon)
-            .setBadgeIconType(NotificationCompat.BADGE_ICON_SMALL);
-}
