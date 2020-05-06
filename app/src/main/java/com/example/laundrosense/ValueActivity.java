cb6dba9d6a0b7f86d2fa7a24dabfadcb9ec10f05
EmmetMack <emack@andrew.cmu.edu>
2020-05-06 16:55:26 -0400

    Should be able to read particle variable and update ui and progress bar, still need actual particle info

--- app/src/main/java/com/example/laundrosense/ValueActivity.java
+++ app/src/main/java/com/example/laundrosense/ValueActivity.java
@@ -1,5 +1,6 @@
 package com.example.laundrosense;
 
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,8 +8,11 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toolbar;
+import android.view.View;
 
 import java.io.IOException;
 import java.util.Timer;
@@ -16,6 +20,7 @@ import java.util.TimerTask;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 import io.particle.android.sdk.cloud.ParticleCloud;
 import io.particle.android.sdk.cloud.exceptions.ParticleCloudException;
@@ -31,6 +36,7 @@ public class ValueActivity extends AppCompatActivity {
     private TextView stage_name;
     private TextView progressValue;
     private int count = 0;
+    private final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,10 +81,37 @@ public class ValueActivity extends AppCompatActivity {
                     @Override
                     public void onSuccess(@NonNull Object i) { // this goes on the main thread
                     //include logic of getting variables in here and counting/checking on them
-
-
-                            stage_name.setText(i.toString());
-
+                        if (i.toString() == "Sensing") {
+                            stage_name.setText("Sensing");
+                            sendNotification("In Sensing stage" );
+                            progressBar.setMax(5);
+                            progressBar.setProgress(1);
+                        } else if (i.toString() == "Wash") {
+                            stage_name.setText("Wash");
+                            sendNotification("In Washing Stage" );
+                            progressBar.setProgress(2);
+                        } else if (i.toString() == "Rinse") {
+                            stage_name.setText("Rinse");
+                            sendNotification("In Rinse stage" );
+                            progressBar.setProgress(3);
+                        } else if (i.toString() == "Spin") {
+                            stage_name.setText("Spin");
+                            sendNotification("In Spin stage" );
+                            progressBar.setProgress(4);
+                        } else if (i.toString() == "Done") {
+                            stage_name.setText("Done");
+                            sendNotification("Washing machine done" );
+                            progressBar.setProgress(5);
+                        } else if (i.toString() == "Dry") {
+                            stage_name.setText("Dry");
+                            sendNotification("Drying" );
+                            progressBar.setMax(2);
+                            progressBar.setProgress((1));
+                        } else if (i.toString() == "Off") {
+                            stage_name.setText("Off");
+                            sendNotification("Dryer finished ");
+                            progressBar.setProgress((2));
+                        }
                     }
 
                     @Override
@@ -89,5 +122,35 @@ public class ValueActivity extends AppCompatActivity {
 
             }
         }, 0, 500);
+    }
+
+    public void sendNotification(String content) {
+
+        //Get an instance of NotificationManager//
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("LaundroSense")
+                        .setContentText(content);
+
+
+        // Gets an instance of the NotificationManager service//
+
+        NotificationManager mNotificationManager =
+
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // When you issue multiple notifications about the same type of event,
+        // it’s best practice for your app to try to update an existing notification
+        // with this new information, rather than immediately creating a new notification.
+        // If you want to update this notification at a later date, you need to assign it an ID.
+        // You can then use this ID whenever you issue a subsequent notification.
+        // If the previous notification is still visible, the system will update this existing notification,
+        // rather than create a new one. In this example, the notification’s ID is 001//
+
+//            NotificationManager.notify();
+
+        mNotificationManager.notify(001, mBuilder.build());
     }
 }
