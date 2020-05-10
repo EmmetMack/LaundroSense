f6ec511b4a8b136b34bfc080063b2424762a0919
EmmetMack <emack@andrew.cmu.edu>
2020-05-10 14:02:12 -0400

    Code should be working, just need to link it to particle data

--- app/src/main/java/com/example/laundrosense/MainActivity.java
+++ app/src/main/java/com/example/laundrosense/MainActivity.java
@@ -42,8 +42,9 @@ import java.util.List;
 import java.util.Random;
 
 import weka.classifiers.Classifier;
-import weka.classifiers.functions.MultilayerPerceptron;
+import weka.classifiers.functions.MultilayerPerceptron.*;
 import weka.classifiers.functions.*;
+import weka.classifiers.trees.RandomForest;
 import weka.core.Attribute;
 import weka.core.DenseInstance;
 import weka.core.Instances;
@@ -78,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         progressValue = findViewById(R.id.estimated_time);
-        stage_name = findViewById((R.id.current_stage));
+        stage_name = findViewById((R.id.stage_value));
 
         valueActivity = new ValueActivity();
 
@@ -91,12 +92,25 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 //valueActivity = new ValueActivity();
                 // we need those for creating new instances later
+
                 final Attribute ax = new Attribute("acclX");
+                final Attribute ax2 = new Attribute("acclX2");
+                final Attribute ax3 = new Attribute("acclX3");
                 final Attribute ay = new Attribute("acclY");
+                final Attribute ay2 = new Attribute("acclY2");
+                final Attribute ay3 = new Attribute("acclY3");
                 final Attribute az = new Attribute("acclZ");
+                final Attribute az2 = new Attribute("acclZ2");
+                final Attribute az3 = new Attribute("acclZ3");
                 final Attribute gy = new Attribute("gyroY");
+                final Attribute gy2 = new Attribute("gyroY2");
+                final Attribute gy3 = new Attribute("gyroY3");
                 final Attribute gx = new Attribute("gyroZ");
+                final Attribute gx2 = new Attribute("gyroZ2");
+                final Attribute gx3 = new Attribute("gyroZ3");
                 final Attribute gz = new Attribute("gyroX");
+                final Attribute gz2 = new Attribute("gyroX2");
+                final Attribute gz3 = new Attribute("gyroX3");
                 final List<String> classes = new ArrayList<String>() {
                     {
                         add("sense");
@@ -110,14 +124,26 @@ public class MainActivity extends AppCompatActivity {
                 };
 
                 // Instances(...) requires ArrayList<> instead of List<>...
-                ArrayList<Attribute> attributeList = new ArrayList<Attribute>(2) {
+                ArrayList<Attribute> attributeList = new ArrayList<Attribute>(1) {
                     {
                         add(ax);
+                        add(ax2);
+                        add(ax3);
                         add(ay);
+                        add(ay2);
+                        add(ay3);
                         add(az);
+                        add(az2);
+                        add(az3);
                         add(gy);
+                        add(gy2);
+                        add(gy3);
                         add(gx);
+                        add(gx2);
+                        add(gx3);
                         add(gz);
+                        add(gz2);
+                        add(gz3);
                         Attribute attributeClass = new Attribute("@@class@@", classes);
                         add(attributeClass);
                     }
@@ -132,11 +158,23 @@ public class MainActivity extends AppCompatActivity {
                 DenseInstance newInstanceStage = new DenseInstance(dataUnpredicted.numAttributes()) {
                     {
                         setValue(ax, valueActivity.ax);
+                        setValue(ax2, Math.pow(valueActivity.ax,2));
+                        setValue(ax3, Math.pow(valueActivity.ax,3));
                         setValue(ay, valueActivity.ay);
+                        setValue(ay2, Math.pow(valueActivity.ay,2));
+                        setValue(ay3, Math.pow(valueActivity.ay, 3));
                         setValue(az, valueActivity.az);
+                        setValue(az2, Math.pow(valueActivity.az,2));
+                        setValue(az3, Math.pow(valueActivity.az, 3));
                         setValue(gx, valueActivity.gx);
+                        setValue(gx2, Math.pow(valueActivity.gx, 2));
+                        setValue(gx3, Math.pow(valueActivity.gx, 3));
                         setValue(gy, valueActivity.gy);
+                        setValue(gy2, Math.pow(valueActivity.gy,2));
+                        setValue(gy3, Math.pow(valueActivity.gy,3));
                         setValue(gz, valueActivity.gz);
+                        setValue(gz2, Math.pow(valueActivity.gz, 2));
+                        setValue(gz3, Math.pow(valueActivity.gz, 3));
                     }
                 };
 
@@ -145,12 +183,12 @@ public class MainActivity extends AppCompatActivity {
                 // reference to dataset
                 newInstance.setDataset(dataUnpredicted);
 
-                MLPClassifier classifier = null;
+                RandomForest classifier = null;
 
                 AssetManager assetManager = getAssets();
 
                 try {
-                    classifier = (MLPClassifier) weka.core.SerializationHelper.read(assetManager.open("Wash-Cycle-Model.model"));
+                    classifier = (RandomForest) weka.core.SerializationHelper.read(assetManager.open("Wash-Cycle-Model.model"));
                 } catch (IOException e) {
                     e.printStackTrace();
                 } catch (Exception e) {
@@ -168,78 +206,85 @@ public class MainActivity extends AppCompatActivity {
 
                     System.out.println("Index of predicted class label: " + result + ", which corresponds to class: " + classes.get(new Double(result).intValue()));
                     String predictedClass = classes.get(new Double(result).intValue());
-                    if (predictedClass == "sensing") {
-                        stage_name.setText("Sensing");
-                        if (sensingCount == 0) {
-                            sendNotification("Sensing stage");
-                            progressBar.setMax(senseBaseline);
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (predictedClass == "sensing") {
+                                stage_name.setText("Sensing");
+                                if (sensingCount == 0) {
+                                    sendNotification("Sensing stage");
+                                    progressBar.setMax(senseBaseline);
 
 
-                        }
-                        progressBar.setProgress(sensingCount);
-                        sensingCount ++;
-                    } else if (predictedClass == "wash") {
-                        stage_name.setText("Wash");
-                        if (washCount == 0) {
-                            sendNotification("In Washing Stage");
-                            progressBar.setProgress(2);
-                        }
-                        if (washCount == 60) {
-                            washBaseline = washBaseline * sensingCount / senseBaseline;
-                            rinseBaseline = rinseBaseline * sensingCount / senseBaseline;
-                            spinBaseline = spinBaseline * sensingCount / senseBaseline;
-                        }
-                        washCount ++;
+                                }
+                                progressBar.setProgress(sensingCount);
+                                sensingCount ++;
+                            } else if (predictedClass == "wash") {
+                                stage_name.setText("Wash");
+                                if (washCount == 0) {
+                                    sendNotification("In Washing Stage");
+                                    progressBar.setProgress(2);
+                                }
+                                if (washCount == 60) {
+                                    washBaseline = washBaseline * sensingCount / senseBaseline;
+                                    rinseBaseline = rinseBaseline * sensingCount / senseBaseline;
+                                    spinBaseline = spinBaseline * sensingCount / senseBaseline;
+                                }
+                                washCount ++;
 
-                    } else if (predictedClass == "rinse") {
-                        stage_name.setText("Rinse");
-                        if (rinseCount == 0) {
-                            sendNotification("Rinsing Now");
-                            progressBar.setProgress(3);
-                        }
-                        if (rinseCount == 60) {
-                            rinseBaseline = rinseBaseline * washCount / washBaseline;
-                            spinBaseline = spinBaseline * washCount / washBaseline;
-                        }
+                            } else if (predictedClass == "rinse") {
+                                stage_name.setText("Rinse");
+                                if (rinseCount == 0) {
+                                    sendNotification("Rinsing Now");
+                                    progressBar.setProgress(3);
+                                }
+                                if (rinseCount == 60) {
+                                    rinseBaseline = rinseBaseline * washCount / washBaseline;
+                                    spinBaseline = spinBaseline * washCount / washBaseline;
+                                }
 
-                        washCount ++;
-                    } else if (predictedClass == "spin") {
-                        stage_name.setText("Spin");
-                        if (spinCount == 0 ) {
-                            sendNotification("In Spin Stage");
-                            progressBar.setProgress(4);
+                                rinseCount ++;
+                            } else if (predictedClass == "spin") {
+                                stage_name.setText("Spin");
+                                if (spinCount == 0 ) {
+                                    sendNotification("In Spin Stage");
+                                    progressBar.setProgress(4);
+                                }
+                                if (spinCount == 60) {
+                                    spinBaseline = spinBaseline * rinseCount / rinseBaseline;
+                                }
+                                spinCount ++;
+                            } else if (predictedClass == "done") {
+                                stage_name.setText("Done");
+                                if (doneCount == 0) {
+                                    sendNotification("Washing machine done!");
+                                    progressBar.setProgress(5);
+                                }
+                                doneCount ++;
+                            } else if (predictedClass == "dry") {
+                                stage_name.setText("Dry");
+                                if (dryingCount == 0) {
+                                    sendNotification("Drying");
+                                    progressBar.setMax(2);
+                                    progressBar.setProgress((1));
+                                }
+                                dryingCount ++;
+                            } else if (predictedClass == "off") {
+                                stage_name.setText("Off");
+                                if (offCount == 0) {
+                                    sendNotification("Dryer is finished!");
+                                    progressBar.setProgress((2));
+                                }
+                                doneCount ++;
+                            }
+                            // in minutes
+                            timeRemaining = ((senseBaseline + washBaseline + rinseBaseline + spinBaseline)
+                                    - (sensingCount + washCount + rinseCount + spinCount)) / 60;
+                            progressValue.setText(timeRemaining+" minutes");
                         }
-                        if (spinCount == 60) {
-                            spinBaseline = spinBaseline * rinseCount / rinseBaseline;
-                        }
-                        spinCount ++;
-                    } else if (predictedClass == "done") {
-                        stage_name.setText("Done");
-                        if (doneCount == 0) {
-                            sendNotification("Washing machine done!");
-                            progressBar.setProgress(5);
-                        }
-                        doneCount ++;
-                    } else if (predictedClass == "dry") {
-                        stage_name.setText("Dry");
-                        if (dryingCount == 0) {
-                            sendNotification("Drying");
-                            progressBar.setMax(2);
-                            progressBar.setProgress((1));
-                        }
-                        dryingCount ++;
-                    } else if (predictedClass == "off") {
-                        stage_name.setText("Off");
-                        if (offCount == 0) {
-                            sendNotification("Dryer is finished!");
-                            progressBar.setProgress((2));
-                        }
-                        doneCount ++;
-                    }
-                    // in minutes
-                    timeRemaining = ((senseBaseline + washBaseline + rinseBaseline + spinBaseline)
-                            - (sensingCount + washCount + rinseCount + spinCount)) / 60;
-                    progressValue.setText(timeRemaining+" minutes");
+                    });
+
 
                 } catch (Exception e) {
                     e.printStackTrace();
